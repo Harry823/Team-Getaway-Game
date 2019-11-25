c9d671483ae801389d7ee8b694aeddf7add61dbe
Harry Tjahja <harry.d.tjahja@gmail.com>
2019-11-25 14:24:32 -0800

    Initial Game Mechanic

---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public string horizontal_axis_name = "Horizontal";
+    //public string vertical_axis_name = "Vertical";
+
+    Rigidbody2D rb;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    public int speed = 5;
+
+    public Vector3 jump;
+    public float jumpForce = .001f;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.deltaTime;
+        float displacement_x = Input.GetAxis(horizontal_axis_name) * speed * time;
+        transform.position += new Vector3(displacement_x, 0, 0);
+
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("The jump button registers");
+            rb.AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * time;
+        }
+        else if(rb.velocity.y > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * time;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Programming-Workshop-2
+
+Hello, VGDC Programmers! Before the workshop, please clone this repo:  
+* `git clone https://github.com/VGDC-UCI/Programming-Workshop-2.git` 
+* Or use Github Desktop / another git frontend
+
+Or download a zip file  
+* Clone or Download -> Download ZIP
+
+Make sure you have [Unity](https://unity3d.com/get-unity/download) on version 2019.2.8f1 or later.
